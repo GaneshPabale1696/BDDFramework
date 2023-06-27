1e53a772560ea5fae5a7edf22e3f05bd6af34ec3
GaneshPabale1696 <GaneshPabale1696@example.com>
2023-06-27 17:32:57 +0530

    Add new code and modify some code

--- src/test/java/StepDefination/StepDef.java
+++ src/test/java/StepDefination/StepDef.java
@@ -1,43 +1,55 @@
 package StepDefination;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import PageObject.AddNewCustomerPage;
 import PageObject.LoginPage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.time.Duration;
+
 import org.junit.Assert;
 
 public class StepDef {
 
 	public WebDriver driver;
-	
 	public LoginPage loginpg;
-	
+	public AddNewCustomerPage addNewCustPg;
+
 	@Given("User Launch Chrome browser")
 	public void user_launch_chrome_browser() {
-		
+
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions ops = new ChromeOptions();
 		ops.addArguments("--remote-allow-origins=*");
 		driver = new ChromeDriver(ops); // launch chrome
 		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		driver.manage().window().maximize();
+
 		loginpg = new LoginPage(driver);
-		
+		addNewCustPg = new AddNewCustomerPage(driver);
+
 	}
 
 	@When("User opens URL {string}")
 	public void user_opens_url(String url) {
-		
+
 		driver.get(url);
-		
+
 	}
 
 	@When("User enters Email as {string} and Password as {string}")
 	public void user_enters_email_as_and_password_as(String emailadd, String password) {
-		
+
 		loginpg.enterEmail(emailadd);
 		loginpg.enterPassword(password);
 	}
@@ -49,9 +61,99 @@ public class StepDef {
 
 	@Then("Page Title should be {string}")
 	public void page_title_should_be(String expected) {
+
+		String actualTitle = driver.getTitle();
+		if (actualTitle.equals(expected)) {
+			Assert.assertTrue(true);// pass
+		} else {
+			Assert.assertTrue(false);// fail
+		}
+	}
+
+	@When("User click on Log out link")
+	public void user_click_on_log_out_link() {
+		loginpg.clickonlogoutbutton();
+
+	}
+
+	@Then("close browser")
+	public void close_browser() {
+
+		driver.close();
+		driver.quit();
+
+	}
+
+	//////// Add New Customers/////////////////////////////////////////
+
+	@Then("User can view Dashboad")
+	public void user_can_view_dashboad() {
+
+		String actualTitle = addNewCustPg.getPageTitle();
+		String expectedTitle = "Dashboard / nopCommerce administration";
+
+		if (actualTitle.equals(expectedTitle)) {
+			Assert.assertTrue(true);
+		} else {
+			Assert.assertTrue(false);
+		}
+
+	}
+
+	@When("User click on customers Menu")
+	public void user_click_on_customers_menu() {
+		addNewCustPg.clickOnCustomersMenu();
+	}
+
+	@When("click on customers Menu Item")
+	public void click_on_customers_menu_item() {
+		addNewCustPg.clickOnCustomersMenuItem();
+	}
+
+	@When("click on Add new button")
+	public void click_on_add_new_button() {
+		addNewCustPg.clickOnAddnew();
+	}
+
+	@Then("User can view Add new customer page")
+	public void user_can_view_add_new_customer_page() {
+
+		String actualTitle = addNewCustPg.getPageTitle();
+		String expectedTitle = "Add a new customer / nopCommerce administration";
+
+		if (actualTitle.equals(expectedTitle)) {
+			Assert.assertTrue(true);// pass
+		} else {
+			Assert.assertTrue(false);// fail
+		}
+
+	}
+
+	@When("User enter customer info")
+	public void user_enter_customer_info() {
+		 addNewCustPg.enterEmail("cs129@gmail.com");
+		//addNewCustPg.enterEmail(generateEmailId() + "@gmail.com");
+		addNewCustPg.enterPassword("test1");
+		addNewCustPg.enterFirstName("Prachi");
+		addNewCustPg.enterLastName("Gupta");
+		addNewCustPg.enterGender("Female");
+		addNewCustPg.enterDob("6/13/1988");
+		addNewCustPg.enterCompanyName("CodeStudio");
+		addNewCustPg.enterAdminContent("Admin content");
+		addNewCustPg.enterManagerOfVendor("Vendor 1");
+
+	}
+
+	@When("click on Save button")
+	public void click_on_save_button() {
+		addNewCustPg.clickOnSave();
+	}
+
+	@Then("User can view confirmation message {string}")
+	public void user_can_view_confirmation_message(String exptectedConfirmationMsg) {
 		
-		String actualTitle=driver.getTitle();
-		if(actualTitle.equals(expected))
+		String bodyTagText = driver.findElement(By.tagName("Body")).getText();
+		if(bodyTagText.contains(exptectedConfirmationMsg))
 		{
 			Assert.assertTrue(true);//pass
 		}
@@ -59,19 +161,6 @@ public class StepDef {
 		{
 			Assert.assertTrue(false);//fail
 		}
-	}
-
-	@When("User click on Log out link")
-	public void user_click_on_log_out_link() {
-		loginpg.clickonlogoutbutton();
-		
-	}
-
-	@Then("close browser")
-	public void close_browser() {
-		
-		driver.close();
-		driver.quit();
 		
 	}
 
--- src/test/java/TestRunner/Run.java
+++ src/test/java/TestRunner/Run.java
@@ -4,17 +4,19 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features = ".//Features/LoginFeature.feature",
+		features = ".//Features/Customer.feature",
 		glue = "StepDefination",
 		dryRun=false,
 		monochrome = true,
-		plugin= {"pretty","junit:Cucumber-reports/reports_xml.xml"
-				,"json:Cucumber-reports/reports_json.json"
-				,"html:Cucumber-reports/reports1.html"}
+		plugin= {"pretty","html:Cucumber-reports/reports1.html"}
 		)
 
 //plugin= {"pretty","json:Cucumber-reports/reports_json.json"}
 //plugin= {"pretty","html:Cucumber-reports/reports1.html"}
+/*plugin= {"pretty","junit:Cucumber-reports/reports_xml.xml"
+		,"json:Cucumber-reports/reports_json.json"
+		,"html:Cucumber-reports/reports1.html"}*/
+
 
 public class Run extends AbstractTestNGCucumberTests{
 	//this class will be empty
